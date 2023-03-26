0298e0ec1f54013f3605e53ba29fd13fc02c3767
pndhkm <panduhakam@gmail.com>
2023-03-26 17:30:23 +0700

    add db-env

--- src/initialproject/databases/db.java
+++ src/initialproject/databases/db.java
@@ -7,19 +7,23 @@ import java.sql.SQLException;
 public class db {
     
     private static final String DRIVER = "org.mariadb.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://103.241.24.82:3306/caranyabegini_login";
-    private static final String USERNAME = "caranyabegini_caranyabegini";
-    private static final String PASSWORD = "jakarta1928!";
-
+    private static final String SERVER = System.getProperty("SERVER");
+    private static final String PORT = System.getProperty("PORT");
+    private static final String DATABASE = System.getProperty("DATABASE");    
+    private static final String URL = "jdbc:mysql://"+SERVER+":"+PORT+"/"+DATABASE ;
+    private static final String USERNAME = System.getProperty("USERNAME");
+    private static final String PASSWORD = System.getProperty("PASSWORD");
+    
+    
     public static Connection buatKoneksi() {
         Connection conn = null;
         try {
             Class.forName(DRIVER);
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Koneksi berhasil");
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);   
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Koneksi gagal");
             e.printStackTrace();
+    
         }
         return conn;
     }
