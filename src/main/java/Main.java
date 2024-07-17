8057ea59ef9781355ca9cc875ab335d9a282b72d
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2024-07-17 22:51:24 +0700

    update templates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,8 @@ public class Main {
             Config.build();
 //            new GenerateUser().start();
 //            new EmployeeView().start();            
-            new LoginView().start();
-//            new MenuView().start();
+//            new LoginView().start();
+            new MenuView().start();
 //            loginView.start();
 //            new CustomerView().start();
 //            new ReportView().start();
